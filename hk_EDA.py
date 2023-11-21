a2492cd48b8d511d10338822aa0258d8f3515bb7
zayunsna <zayunsna@gmail.com>
2023-11-21 17:15:15 +0900

    Update EDA & DA utils | new upload XGBoost Example

--- hk_EDA.py
+++ hk_EDA.py
@@ -10,7 +10,6 @@ from sklearn.preprocessing import MinMaxScaler
 import seaborn as sns
 import array
 
-
 ################################################################################################################################
 #### This part is making the dataframe sample for the function test.
 import random
@@ -42,131 +41,113 @@ for col in df.columns:
     df.loc[df.sample(frac=frac).index, col] = np.nan
 #### sample dataframe part end.
 ################################################################################################################################
-file_path = '../DevOps_MLOps_Tutorial_class/data/spaceship-titanic/train.csv'
-df2 = pd.read_csv(file_path)
 
-df2[["Deck", "Cabin_num", "Side"]] = df2["Cabin"].str.split("/", expand=True)
-df2 = df2.drop('Cabin', axis=1)
-df2 = df2.drop('PassengerId', axis=1)
-df2 = df2.drop('Name', axis=1)
-#### sample dataframe-2 part end.
-################################################################################################################################
+class DataAnalysisTool:
+    def __init__(self, dataset):
+        self.dataset = dataset
 
-def create_grid(n):
-    root = n ** 0.5
-    rows = int(root) if root.is_integer() else int(root) + 1
-    cols = rows if rows * (rows - 1) < n else rows - 1
-    return rows, cols
+    @staticmethod
+    def create_grid(n):
+        root = n ** 0.5
+        rows = int(root) if root.is_integer() else int(root) + 1
+        cols = rows if rows * (rows - 1) < n else rows - 1
+        return rows, cols
 
-def getInfo(dataset : pd.DataFrame):
-    print("\n### Data Summary")
-    print(dataset.describe())
-    print("\n### Summarized data")
-    print(dataset.info())
-    print("\n### Data Head info")
-    print(dataset.head())
+    def get_info(self):
+        print("\n### Data Summary")
+        print(self.dataset.describe())
+        print("\n### Summarized data")
+        print(self.dataset.info())
+        print("\n### Data Head info")
+        print(self.dataset.head())
 
-def columnsInfo(dataset: pd.DataFrame, type:array = []):
-    column = dataset.select_dtypes(include=type)
-    column_name = column.columns
-    column_count = len(column_name)
-    return column_name, column_count
+    def columns_info(self, types):
+        column = self.dataset.select_dtypes(include=types)
+        column_name = column.columns
+        column_count = len(column_name)
+        return column_name, column_count
 
-def drawPlot(type:string, grid_x:int, grid_y:int, dataset:pd.DataFrame, entry:int, item:array, nbins : int = 50, ylog : bool = False):
-    fig, ax = plt.subplots(grid_x, grid_y, figsize = (10, 10))
-    ax = ax.flatten()
-    for i in range(entry):
-        ax[i].grid()
-        if ylog: ax[i].set_yscale('log')
-        if type == 'num': sns.histplot(dataset[item[i]], color='b', bins=nbins, ax=ax[i])
-        else: sns.countplot(x=dataset[item[i]], ax=ax[i])
-    plt.show()
+    def draw_plot(self, plot_type, grid_x, grid_y, entry, items, nbins=50, ylog=False):
+        fig, ax = plt.subplots(grid_x, grid_y, figsize=(10, 10))
+        ax = ax.flatten()
+        for i in range(entry):
+            ax[i].grid()
+            if ylog: 
+                ax[i].set_yscale('log')
+            if plot_type == 'num': 
+                sns.histplot(self.dataset[items[i]], color='b', bins=nbins, ax=ax[i])
+            else: 
+                sns.countplot(x=self.dataset[items[i]], ax=ax[i])
+        plt.show()
 
-def numericalPlot(dataset : pd.DataFrame, nbins : int = 50, ylog : bool = False):
-    numerical_column_name, column_count = columnsInfo(dataset, ['int', 'float'])
-    grid_x, grid_y = create_grid(column_count)
-    drawPlot("num", grid_x, grid_y, dataset, column_count, numerical_column_name, nbins, ylog)
+    def numerical_plot(self, nbins=50, ylog=False):
+        numerical_column_name, column_count = self.columns_info(['int', 'float'])
+        grid_x, grid_y = DataAnalysisTool.create_grid(column_count)
+        self.draw_plot("num", grid_x, grid_y, column_count, numerical_column_name, nbins, ylog)
 
-def categoricalPlot(dataset : pd.DataFrame):
-    categorical_column_name, column_count = columnsInfo(dataset, ['object'])
-    grid_x, grid_y = create_grid(column_count)
-    drawPlot("category", grid_x, grid_y, dataset, column_count, categorical_column_name)
-    
-def setMissingValue(dataset: pd.DataFrame, method:string = "random"):
-    ## TODO : Need to build how control the different cases
-    return 0
+    def categorical_plot(self):
+        categorical_column_name, column_count = self.columns_info(['object'])
+        grid_x, grid_y = DataAnalysisTool.create_grid(column_count)
+        self.draw_plot("category", grid_x, grid_y, column_count, categorical_column_name)
 
-def setOneHotEncoding(dataset: pd.DataFrame, item:array) -> pd.DataFrame:
-    for name in item:
-        encoded_column = pd.get_dummies(dataset[name], prefix=name, drop_first=False)
-        dataset = dataset.drop(name, axis=1)
-        dataset = pd.concat([dataset, encoded_column], axis = 1)
-    return dataset
+    @staticmethod
+    def scaler(data):
+        sc = MinMaxScaler(feature_range=(0, 1))
+        sc = sc.fit(data)
+        scaled_data = sc.transform(data)
+        return pd.DataFrame(scaled_data, columns=data.columns)
 
-def scaler(dataset:pd.DataFrame):
-    sc = MinMaxScaler(feature_range=(0,1))
-    sc = sc.fit(dataset)
-    scaled_data = sc.transform(dataset)
-    return pd.DataFrame(scaled_data, columns=dataset.columns)
+    @staticmethod
+    def cal_pcc(feature_a, feature_b, _range):
+        fitter, C_p = np.polyfit(feature_a, feature_b, 1, cov=True)
+        
+        xModel = np.linspace(_range[0][0], _range[0][1], 500)
+        yModel = np.polyval(fitter, xModel)
 
-def calPCC(feature_a:np.ndarray, feature_b:np.ndarray, _range : list):
-    fitter, C_p = np.polyfit(feature_a, feature_b, 1, cov=True)
-    
-    xModel = np.linspace(_range[0][0], _range[0][1], 500)
-    yModel = np.polyval(fitter, xModel)
+        TT = np.vstack([xModel**(1-i) for i in range(2)]).T
+        yi = np.dot(TT, fitter)
+        C_yi = np.dot(TT, np.dot(C_p, TT.T))
+        sig_yi = np.sqrt(np.diag(C_yi))
 
-    TT = np.vstack([xModel**(1-i) for i in range(2)]).T
-    yi = np.dot(TT, fitter)
-    C_yi = np.dot(TT, np.dot(C_p, TT.T))
-    sig_yi = np.sqrt(np.diag(C_yi))
+        return xModel, yModel, yi, sig_yi
 
-    return xModel, yModel, yi, sig_yi
+    def get_feature_correlation(self, feature_a, feature_b, _range):
+        xData = self.dataset[feature_a]
+        yData = self.dataset[feature_b]
+        xModel, yModel, yi, sig_yi = DataAnalysisTool.cal_pcc(xData, yData, _range)
 
-def getFeatureCorrelation(dataset: pd.DataFrame, feature_a:str, feature_b:str, _range:list):
-    xData = dataset[feature_a]
-    yData = dataset[feature_b]
-    xModel, yModel, yi, sig_yi = calPCC(xData, yData, _range)
+        fig = plt.figure(figsize=(8, 6), dpi=100)
+        fig = fig.add_subplot(111)
 
-    fig = plt.figure(figsize=(8,6), dpi=100)
-    fig = fig.add_subplot(111)
+        plt.hist2d(x=xData, y=yData, bins=50, norm=mpl.colors.LogNorm(), range=_range)
+        fig.plot(xModel, yModel)
+        fig.fill_between(xModel, yi+sig_yi, yi-sig_yi, alpha=.25)
+        plt.colorbar()
+        plt.grid()
+        plt.xlabel(feature_a)
+        plt.ylabel(feature_b)
+        plt.show()
 
-    plt.hist2d(x = xData, y = yData, bins=50, norm=mpl.colors.LogNorm(), range = _range)
-    fig.plot(xModel, yModel)
-    fig.fill_between(xModel, yi+sig_yi, yi-sig_yi, alpha=.25)
-    plt.colorbar()
-    plt.grid()
-    plt.xlabel(feature_a)
-    plt.ylabel(feature_b)
-    plt.show()
+    def get_feature_heatmap(self):
+        target_column_name, _ = self.columns_info(['int', 'float'])
+        numerical_dataset = self.dataset[target_column_name]
+        scaled_numerical_dataset = DataAnalysisTool.scaler(numerical_dataset).corr()
+        plt.figure(figsize=(14, 10))
+        sns.heatmap(scaled_numerical_dataset, cmap='Blues', vmin=-1, vmax=1, annot=True)
+        plt.xticks(rotation=45, ha='right')
+        plt.show()
 
-def getFeatureHeatmap(dataset: pd.DataFrame):
-    target_column_name, _ = columnsInfo(dataset, ['int', 'float'])
-    numerical_dataset = dataset[target_column_name]
-    scaled_numerical_dataset= scaler(numerical_dataset).corr()
-    plt.figure(figsize=(14, 10))
-    plot = sns.heatmap(scaled_numerical_dataset, cmap='Blues', vmin=-1, vmax=1, annot=True)
-    plt.xticks(rotation=45, ha='right')
-    plt.show()
+    def remove_columns(self, column_names):
+        for name in column_names:
+            self.dataset = self.dataset.drop(name, axis=1)
+        return self.dataset
 
-def removeColumns(dataset: pd.DataFrame, colume_name:array = []) -> pd.DataFrame:
-    for i in range(len(colume_name)):
-        dataset = dataset.drop(colume_name[i], axis=1)
-    return dataset
-
+# Example usage
 # df = numpy.random # Test data
-# df2 = kaggle example data
 
-# getInfo(df2)
-# numericalPlot(df2, 20)
-# numericalPlot(df2, 20, True)
-# categoricalPlot(df2)
-# print(df2.head())
-# print("#"*50)
-# df3 = setOneHotEncoding(df2, ['HomePlanet', 'Destination', 'Deck', 'Side'])
-# print(df3[:10])
-# print(df3.loc[:10, 'Transported':'Side_S'])
-
-# _range = [[0,15000],[0,30000]]
-# df2[['RoomService', 'FoodCourt']] = df2[['RoomService', 'FoodCourt']].fillna(value=0)
-# getFeatureCorrelation(df2, 'RoomService', 'FoodCourt', _range)
-# getFeatureHeatmap(df2)
+# data_tool = DataAnalysisTool(df)
+# data_tool.get_info()
+# data_tool.numerical_plot()
+# data_tool.categorical_plot()
+# data_tool.get_feature_heatmap()
+# data_tool.remove_columns(['column1', 'column2'])
--- hk_simple_DA.py
+++ hk_simple_DA.py
@@ -11,10 +11,13 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 from pandas.plotting import scatter_matrix
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from scipy.stats import multivariate_normal
 import seaborn as sns
 
 def elastic_naming(form:str, n:int):
@@ -38,58 +41,206 @@ def make_testdf(n_samples:int, nFeatures:int):
     return df_scaled
 ################################################################################################################################
 
+class PCA_Analysis:
+    def __init__(self, n_components=2):
+        self.n_components = n_components
+        self.pca = PCA(n_components=n_components)
+        self.scaler = StandardScaler()
+        self.explained_variance = None
+        self.cumulative_explained_variance = None
+        self.pca_df = None
+        self.principal_components = None
 
-## PCA
-def applyPCA(dataset:pd.DataFrame, n_components:int=2, drawing:bool = False):
-    scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(dataset)
+    def fit(self, dataset):
+        data_scaled = self.scaler.fit_transform(dataset)
+        self.principal_components = self.pca.fit_transform(data_scaled)
+
+        # Explained variance ratio
+        self.explained_variance = self.pca.explained_variance_ratio_
+        print(f"Explained Variance Ratio: {self.explained_variance}")
+
+        # Cumulative explained variance
+        self.cumulative_explained_variance = np.cumsum(self.explained_variance)
+        print(f"Cumulative Explained Variance: {self.cumulative_explained_variance}")
+
+        column_names = self.elastic_naming(form='Principal Component', n=self.n_components)
+        self.pca_df = pd.DataFrame(data=self.principal_components, columns=column_names)
+
+        return self
+
+    def plot_pca(self):
+        if self.pca_df is not None:
+            plt.figure(figsize=(14, 6))
+
+            plt.subplot(1, 2, 1)
+            plt.bar(range(1, len(self.explained_variance) + 1), self.explained_variance, alpha=0.5, align='center', label='Individual explained variance')
+            plt.step(range(1, len(self.cumulative_explained_variance) + 1), self.cumulative_explained_variance, where='mid', label='Cumulative explained variance')
+            plt.ylabel('Explained variance ratio')
+            plt.xlabel('Principal component index')
+            plt.legend(loc='best')
+            plt.title('Scree Plot')
+
+            plt.subplot(1, 2, 2)
+            plt.scatter(self.pca_df.iloc[:, 0], self.pca_df.iloc[:, 1])
+            plt.xlabel(self.pca_df.columns[0])
+            plt.ylabel(self.pca_df.columns[1])
+            plt.title('2 component PCA')
+
+            plt.tight_layout()
+            plt.show()
+        else:
+            print("PCA not fitted yet.")
+
+    def get_transformed_data(self):
+        return self.pca_df
+
+    def elastic_naming(self, form, n):
+        return [f"{form} {i + 1}" for i in range(n)]
+
+
+# == Test code of PCA
+# df_scaled = make_testdf(n_samples=1000, nFeatures=5)
+# pca = PCA_Analysis(n_components=3)
+# pca.fit(df_scaled)
+# pca.plot_pca()
+# transformed_data = pca.get_transformed_data()
+# print(transformed_data)
+
+
+############### GMM (with K-Means)
+class GMM_Analysis:
+    def __init__(self, n_components, n_iter, tol):
+        # Initialize the Gaussian Mixture Model.
+        # Parameters:
+        # -> n_components (int): Number of Gaussian distributions (clusters).
+        # -> n_iter (int): Maximum number of iterations for the EM algorithm.
+        # -> tol (float): Tolerance for convergence.
+        self.n_components = n_components
+        self.n_iter = n_iter
+        self.tol = tol
+        self.means = None
+        self.covariances = None
+        self.weights = None
+        self.converged = False
+
+    def initialize_parameters(self, X):
+        # Initialize the parameters using K-Means for initial guesses.
+
+        # Parameters:
+        # -> X (ndarray): Data points.
+
+        # Using K-Means to initialize means
+        kmeans = KMeans(n_clusters=self.n_components)
+        kmeans.fit(X)
+        self.means = kmeans.cluster_centers_
+
+        # Initialize covariances and weights
+        self.covariances = [np.cov(X.T) for _ in range(self.n_components)]
+        self.weights = np.ones(self.n_components) / self.n_components
+
+    def e_step(self, X):
+        # E-step of the EM algorithm. Calculate responsibilities.
+        # Parameters:
+        # -> X (ndarray): Data points.
+
+        # Returns:
+        #   responsibilities (ndarray): Probability of each data point belonging to each cluster.
+        responsibilities = np.zeros((X.shape[0], self.n_components))
+
+        for k in range(self.n_components):
+            rv = multivariate_normal(self.means[k], self.covariances[k])
+            responsibilities[:, k] = self.weights[k] * rv.pdf(X)
+
+        responsibilities /= responsibilities.sum(axis=1, keepdims=True)
+        return responsibilities
+
+    def m_step(self, X, responsibilities):
+        # M-step of the EM algorithm. Update parameters based on responsibilities.
+        # Parameters:
+        # -> X (ndarray): Data points.
+        # -> responsibilities (ndarray): Responsibilities from the E-step.
+        Nk = responsibilities.sum(axis=0)
+
+        # Update means
+        self.means = np.dot(responsibilities.T, X) / Nk[:, np.newaxis]
+
+        # Update covariances
+        for k in range(self.n_components):
+            diff = X - self.means[k]
+            self.covariances[k] = np.dot(responsibilities[:, k] * diff.T, diff) / Nk[k]
+
+        # Update weights
+        self.weights = Nk / X.shape[0]
+
+    def fit(self, X):
+        # Fit the GMM to the data.
+        # Parameters:
+        # -> X (ndarray): Data points.
+        self.initialize_parameters(X)
+
+        log_likelihood_old = 0
+        for _ in range(self.n_iter):
+            # E-step
+            responsibilities = self.e_step(X)
+
+            # M-step
+            self.m_step(X, responsibilities)
+
+            # Check for convergence
+            log_likelihood_new = np.sum(np.log(np.sum([k * multivariate_normal(self.means[k], self.covariances[k]).pdf(X) for k in range(self.n_components)], axis=0)))
+            if np.abs(log_likelihood_new - log_likelihood_old) < self.tol:
+                self.converged = True
+                break
+            log_likelihood_old = log_likelihood_new
+
+        return self
+
+    def predict(self, X):
+        # Predict the cluster for each data point.
+        # Parameters:
+        # -> X (ndarray): Data points.
+        # Returns:
+        # -> labels (ndarray): Cluster labels for each data point.
+        responsibilities = self.e_step(X)
+        return np.argmax(responsibilities, axis=1)
     
-    pca = PCA(n_components=n_components)
-    principal_components = pca.fit_transform(data_scaled)
+    def plot_gmm(self, X):
+        # Plot data points
+        plt.scatter(X[:, 0], X[:, 1], s=15)
 
-    # Explained variance ratio
-    explained_variance = pca.explained_variance_ratio_
-    print(f"Explained Variance Ratio: {explained_variance}")
-    # Cumulative explained variance
-    cumulative_explained_variance = np.cumsum(explained_variance)
-    print(f"Cumulative Explained Variance: {cumulative_explained_variance}")
+        # Plot the Gaussian components
+        for mean, covar in zip(self.means, self.covariances):
+            # Calculate the eigenvalues and eigenvectors for the covariance matrix
+            v, w = np.linalg.eigh(covar)
+            v = 2. * np.sqrt(2.) * np.sqrt(v)
+            u = w[0] / np.linalg.norm(w[0])
 
-    column_names = elastic_naming(form='Principal Component', n=n_components)
-    # pca_df = pd.DataFrame(data=principal_components, columns=['Principal Component 1', 'Principal Component 2'])
-    pca_df = pd.DataFrame(data=principal_components, columns=column_names)
+            # Calculate angle in degrees for the ellipse
+            angle = np.arctan2(u[1], u[0])
+            angle = np.degrees(angle)  # Convert to degrees
 
-    if drawing:
-        plt.figure(figsize=(14,6))
-        plt.subplot(1, 2, 1)
-        plt.bar(range(1, len(explained_variance) + 1), explained_variance, alpha=0.5, align='center', label='Individual explained variance')
-        plt.step(range(1, len(cumulative_explained_variance) + 1), cumulative_explained_variance, where='mid', label='Cumulative explained variance')
-        plt.ylabel('Explained variance ratio')
-        plt.xlabel('Principal component index')
-        plt.legend(loc='best')
-        plt.title('Scree Plot')
+            ell = Ellipse(xy=mean, width=v[0], height=v[1], angle=angle, edgecolor='black')
+            ell.set_clip_box(plt.gca().bbox)
+            ell.set_alpha(0.5)
+            plt.gca().add_artist(ell)
 
-        plt.subplot(1, 2, 2)
-        plt.scatter(pca_df[column_names[0]], pca_df[column_names[1]])
-        plt.xlabel(column_names[0])
-        plt.ylabel(column_names[1])
-        plt.title('2 component PCA')
-
-        plt.tight_layout()
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.title('Gaussian Mixture Model')
         plt.show()
 
-    return pca_df, principal_components, pca
+# == Test code of GMM
+# X = make_testdf(n_samples=1000, nFeatures=5)
+# gmm = GMM_Analysis(n_components=3, n_iter=100, tol=1e-3)
+# gmm.fit(X)  # X is your data
+# gmm.plot_gmm(X)
+# labels = gmm.predict(X)
+# print(labels)
 
-df_scaled = make_testdf(n_samples=1000, nFeatures=5)
-pca_df, pc, pca = applyPCA(dataset=df_scaled, n_components=5, drawing=True)
-print(pca_df.head())
+############### Simple Linear Regression
 
+############### Multiple Linear Regression
 
-## GMM (with K-Means)
+############### Lasso : If a specific feature is important than all, Lasso might gives better analysis result.
 
-## Simple Linear Regression
-
-## Multiple Linear Regression
-
-## Lasso : If a specific feature is important than all, Lasso might gives better analysis result.
-
-## Ridge : If all feature is important uniformly, Ridge can gives better analysis performance than Lasso.
+############### Ridge : If all feature is important uniformly, Ridge can gives better analysis performance than Lasso.
